192b7d76a0e1a12be5a21e4456bf4fab0c01c03a
Vladyslav Khomenko <vladislav.homenko@gmail.com>
2025-01-27 15:07:26 +0200

    Added horizontalAccuracy, orientationYawAccuracy and verticalAccuracy to camera geospatial pose

--- lib/src/arcore_geospatial_pose.dart
+++ lib/src/arcore_geospatial_pose.dart
@@ -3,11 +3,17 @@ class ArcoreGeospatialPose {
   double? longitude;
   double? altitude;
   List<double>? eastUpSouthQuaternion;
+  double? horizontalAccuracy;
+  double? orientationYawAccuracy;
+  double? verticalAccuracy;
 
   ArcoreGeospatialPose.fromMap(Map<dynamic, dynamic> map) {
     this.latitude = map['latitude'];
     this.longitude = map['longitude'];
     this.altitude = map['altitude'];
     this.eastUpSouthQuaternion = map['eastUpSouthQuaternion'];
+    this.horizontalAccuracy = map['horizontalAccuracy'];
+    this.orientationYawAccuracy = map['orientationYawAccuracy'];
+    this.verticalAccuracy = map['verticalAccuracy'];
   }
 }
